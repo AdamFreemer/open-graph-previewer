ea9e0ba6d102f08122a73d7af2e8de55e261d1a3
Adam Freemer <adam@freemer.com>
2020-09-06 15:56:51 -0400

    Stub in basic open graph image update on page and back-end for job

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def index
   end
+
+  def create
+    og_image_url = "http://www.google.com"
+    render json: { og_image_url: og_image_url }
+  end
 end
--- app/javascript/packs/pages.js
+++ app/javascript/packs/pages.js
@@ -2,5 +2,20 @@ $(document).ready(function() {
   $("#query-page-button").click(function() {
     console.log("click!")
     $("#query-page-button").html("<span><i class='fa fa-cog fa-spin'></i>&nbsp;Working...</span>"); 
+    let url = { url: encodeURIComponent($('#input-url').val()) }
+
+    updateProcessedJob();
+
+    $.post( "/pages/", url, function(data) {
+      $(".result").html(data);
+      console.log('== data: ' + data['og_image_url'])
+    }); 
+
+    $("#og_image").attr("src", "https://www.apple.com/ac/structured-data/images/open_graph_logo.png?201809210816");
+
   });
 });
+
+function updateProcessedJob() {
+  console.log("== background job awaiting...")
+}
