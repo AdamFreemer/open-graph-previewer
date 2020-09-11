c22633919bd7776b6bd248081a642d496e6f5e6a
Adam Freemer <a@freemer.com>
2020-09-11 00:11:39 -0400

    Refactoring of check background job js

--- app/javascript/packs/pages.js
+++ app/javascript/packs/pages.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
   $("#query-page-button").click(function() {
-    console.log("click!")
     $("#query-page-button").html("<span><i class='fa fa-cog fa-spin'></i>&nbsp;Working...</span>"); 
     let url = { url: encodeURIComponent($('#input-url').val()) }
 
@@ -9,22 +8,20 @@ $(document).ready(function() {
 
       checkProcessedJob(data['page_id']);
     }); 
-
-    // run this until og_image is populated ever 2s
   });
 });
 
 function checkProcessedJob(page_id) {
-  console.log("== background job awaiting...")
-  $.get( "/pages/" + page_id, function(data) {
+  var jqxhr = $.get( "/pages/" + page_id, function(data) {
     $(".result").html(data);
+  })
 
-    console.log("data['og_image_url']: " + data['og_image_url'])
-    console.log("data['processed']: " + data['processed'])
+  jqxhr.always(function(data) {
+    console.log("== data['og_image_url']: " + data['og_image_url'])
+    console.log("== data['processed']: " + data['processed'])
     $('#og_url').text(`${data['og_image_url']}` );
-
-
     $("#og_image").attr("src", data['og_image_url']);
     $("#query-page-button").html("<span></i>Submit</span>"); 
-  }); 
+    console.log( "== always callback executed" );
+  });
 }
